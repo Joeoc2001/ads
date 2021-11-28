57338a23ebc2e1d22712777b1f54b828826e3c46
Joe O'Connor <joeoc2625@gmail.com>
2021-11-28 20:25:24 +0000

    Fix hyphen issue

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = "ads_jo429"
+NAME = "adsjo429"
 DESCRIPTION = "Files for performing fynesse pipeline data analysis with python."
 URL = "https://github.com/jo429/ads"
 EMAIL = "jo429@cam.ac.uk"
